51d78f3099923d1e2340f055e07ef782e2d13542
Andrey Sergeyev <savilit@yandex.ru>
2023-05-28 20:28:17 +0300

    feat: Add start/stop script's

--- start.sh
+++ start.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
 
-# Проверка наличия приложения openssl
-if ! which openssl > /dev/null; then
-  echo "Error: OpenSSL is not installed"
-  echo "To install OpenSSL, please use your package manager, for example:"
-  echo "  - On Debian/Ubuntu: sudo apt-get install openssl"
-  echo "  - On Fedora/Red Hat/CentOS: sudo yum install openssl"
-  echo "  - On macOS (with Homebrew installed): brew install openssl"
-  exit 1
+# Проверка установлен ли Docker
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Docker не установлен. Установка...'
+  sudo apt-get update
+  sudo apt-get install -y docker.io
 fi
 
-# Проверка наличия файла .env
+# Проверка установлен ли Docker Compose
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Docker Compose не установлен. Установка...'
+  sudo apt-get update
+  sudo apt-get install -y docker-compose
+fi
+
+# Проверка установлен ли OpenSSL
+if ! [ -x "$(command -v openssl)" ]; then
+  echo 'OpenSSL не установлен. Установка...'
+  sudo apt-get update
+  sudo apt-get install -y openssl
+fi
+
+# Проверка существует ли файл .env в текущем каталоге
 if [ ! -f .env ]; then
-  # Создание файла .env
+  echo 'Создание файла .env...'
   touch .env
+fi
+
+# Проверка установлена ли переменная API_BASE_URL в файле .env и соответствует ли она указанным правилам
+api_base_url=$(grep "API_BASE_URL" .env | cut -d '=' -f2)
+if [[ -z $api_base_url ]] || [[ $api_base_url != https:* ]] || [[ $api_base_url == */ ]]; then
+  # Запрос значения переменной API_BASE_URL у пользователя
+  while true; do
+    read -p "Пожалуйста, введите значение для API_BASE_URL (должно начинаться с https: и не заканчиваться на /): " api_base_url
+    if [[ $api_base_url == https:* ]] && [[ $api_base_url != */ ]]; then
+      break
+    else
+      echo "Неверное значение. Попробуйте еще раз."
+    fi
+  done
+
+  # Запись переменной API_BASE_URL в файл .env
+  sed -i "/API_BASE_URL/d" .env # Удаление старого значения переменной API_BASE_URL из файла .env (если оно есть)
+  echo "API_BASE_URL=$api_base_url" >> .env # Добавление нового значения переменной API_BASE_URL в файл .env
 fi
 
 # Проверка наличия и длины секретной строки в файле .env
