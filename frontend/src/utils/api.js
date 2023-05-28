7bd886ac8f0bb1bcd51148ff8c201879393da924
Andrey Sergeyev <savilit@yandex.ru>
2023-05-29 00:47:33 +0300

    hotfix:fix REACT_APP_base_api_url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -87,7 +87,8 @@ class Api {
 
 const api = new Api({
   baseUrl:
-    process.env.API_BASE_URL || 'https://mesto.nomoreparties.co/v1/cohort-60',
+    process.env.REACT_APP_API_BASE_URL ||
+    'https://mesto.nomoreparties.co/v1/cohort-60',
   headers: {
     authorization: '91dc4eb1-7a8e-4261-b2c1-7c75534621a0',
     'Content-Type': 'application/json',
