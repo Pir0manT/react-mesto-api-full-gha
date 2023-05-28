290c06745903352e203d007380534ab0cc5d6b10
Andrey Sergeyev <savilit@yandex.ru>
2023-05-28 17:45:04 +0300

    feat: new features in docker-compose and app.js

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const { errors } = require('celebrate')
+const { dotenv } = require('dotenv')
 const routes = require('./routes')
 const errorsHandler = require('./middlewares/handelError')
 
-require('dotenv').config()
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config()
+}
 
-const { PORT = 3000, DB_PATH = 'mongodb://mongo:27017/mestodb' } =
-  process.env
+const { PORT = 3000, DB_PATH = 'mongodb://mongo:27017/mestodb' } = process.env
 
 const app = express()
 
@@ -22,6 +24,14 @@ app.use(errors())
 app.use(errorsHandler)
 
 mongoose.connect(DB_PATH)
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('Connected to MongoDB')
+})
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 })
